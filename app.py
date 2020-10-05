56e1791526f1aaabdf6211faf1d9021e20e2db85
xz3t <leu.valentin@gmail.com>
2020-10-05 11:49:58 +0000

    Connect app to MongoDB database

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'weeklyShopping'
+app.config["MONGO_URI"] = 'mongodb+srv://root:l3tm3in@myfirstcluster.kmobf.mongodb.net/weeklyShopping?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def setupflask():
-    return 'Flask app works!'
+@app.route('/shopping_list')
+def shopping_list():
+    return render_template("shopping_list.html", lists=mongo.db.shopping_list.find())
 
 
 if __name__ == '__main__':
