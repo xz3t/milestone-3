05806434f973772e043d3c95673ca1a938d5ab3d
xz3t <leu.valentin@gmail.com>
2020-10-05 12:05:40 +0000

    Hide URL to database

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'weeklyShopping'
-app.config["MONGO_URI"] = 'mongodb+srv://root:l3tm3in@myfirstcluster.kmobf.mongodb.net/weeklyShopping?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
