eae2171f68123ffbb857b908889de3aa0602bd40
xz3t <leu.valentin@gmail.com>
2020-10-07 14:42:22 +0000

    Route for add_recipe created

--- app.py
+++ app.py
@@ -68,6 +68,11 @@ def recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())    
 
 
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html", 
+        recipes=mongo.db.recipes.find())
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
